adf9b07e41ccb225fbbe665597e80cd720f82fd0
charlesdowd <charlie.dowd96@gmail.com>
2020-08-23 18:43:31 +0800

    set up a basic express server + nodemon

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const volleyball = require('volleyball');
+
+/* 
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+morgan is an http request logger middlewear, helps us debug 
+(log each incoming request)instead we will use volleyball, 
+which essentially does the same thing but also logs the response as
+*/
+
+const app = express();
+
+app.use(volleyball);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World! Nodemon is working!'
+  });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
